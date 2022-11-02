89b2b0b039e44a4940f1e8705b258b8c10102818
blinxduh <111649405+blinxduh@users.noreply.github.com>
2022-11-02 20:36:11 +0000

    Update UpvalueScanner.lua

--- UpvalueScanner.lua
+++ UpvalueScanner.lua
@@ -1,1 +1,13 @@
+local scanner = {}
 
+scanner["get"] = function(method) 
+    for index, value in pairs(debug.getupvalues(method)) do 
+        print("--INDEX:",index,"--VALUE:",value)
+    end
+end
+
+scanner["set"] = function(method, index, newValue) 
+    debug.setupvalue(method, index, newValue)
+end 
+
+return scanner
